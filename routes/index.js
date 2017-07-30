f9f485ffb0b9882590c1a2964be37c293af7c9d8
Michael Salaverry <michaels@wix.com>
2017-07-30 17:16:10 +0300

    working when deployed to now on node 8

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const giphySearcher = require('../../giphyFeelingLucky/src/index');
+const fetch = require('node-fetch');
+const apiKey = process.env.giphyapikey;
+
+function giphySearchURL(query) {
+    return `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=1&offset=0&rating=G&lang=en`;
+}
+
+function fetchFirstGiphyResult(query) {
+    return fetch(`${giphySearchURL(query)}`)
+        .then(res=>res.json())
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,8 +18,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/search',function (req, res, next) {
+    console.log(apiKey);
   console.log(req.query.query);
-  giphySearcher.fetchFirstGiphyResult(req.query.query)
+  fetchFirstGiphyResult(req.query.query)
       .then(fetchedData => res.render('result', {imageURL:fetchedData.data[0].images.preview_gif.url}))
 })
 
